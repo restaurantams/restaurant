5b9988e541484f3306826a9ae71e86fbbab6c801
alumnat <alumnat@DESKTOP-KS8A00N>
2019-04-15 07:27:45 +0200

    connexio

---
+++ restaurantAMS/connexio.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "db5000047478.hosting-data.io";
+$database = "dbs42384";
+$username = "dbu113975";
+$password = "prueb@sTIC006";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_close($conn);
+?>
--- restaurantAMS/index.php
+++ restaurantAMS/index.php
@@ -11,7 +11,18 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        $servername = "db5000047478.hosting-data.io";
+$database = "dbs42384";
+$username = "dbu113975";
+$password = "prueb@sTIC006";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_close($conn);
         ?>
     </body>
 </html>
