01d1b08cc7a33e9cf317e55745b034bdcc406798
alumnat <alumnat@DESKTOP-KS8A00N>
2019-04-30 12:37:04 +0200

    plantilla

--- restaurantAMS/index.php
+++ restaurantAMS/index.php
@@ -10,19 +10,6 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php
-        $servername = "db5000047478.hosting-data.io";
-$database = "dbs42384";
-$username = "dbu113975";
-$password = "prueb@sTIC006";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
-mysqli_close($conn);
-        ?>
+
     </body>
 </html>
---
+++ restaurantAMS/restaurantams.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+  <title>RestaurantAMS</title>
+  <link rel="stylesheet" type="text/css" href="estilsrestaurant.css">
+</head>
+
+<body>
+
+<h1>RestaurantAMS</h1>
+  <img class="jj" src="LOGO EMPRESA.png">
+<nav>
+  <a href="#">Inici</a>
+  <a href="#">Menú</a>
+  <a href="#">Reserves</a>
+  <a href="#">Galeria</a>
+  <a href="#">Contacte</a>
+  <div class="animation start-home"></div>
+</nav>
+
+<p>
+   <span>RestaurantAMS
+</p>
+
+</body>
+</html>
